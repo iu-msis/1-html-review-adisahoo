c54103be0d7696d521451f92750debc0847bd51b
adisahoo <adisahoo@iu.edu>
2021-09-20 13:35:49 -0400

    modified birthdate with day.js

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -5,6 +5,12 @@ const Offer = {
             "person":{}
         }
     },
+    computed: {
+        prettyBirthday(){
+            return dayjs(this.person.dob.date).format('D MMM YYYY');
+        }
+    },
+    
     created(){          //event book funtion "created()" by vue.js allows you to attach to the event
         fetch('https://randomuser.me/api/')
         .then(response => response.json())        
