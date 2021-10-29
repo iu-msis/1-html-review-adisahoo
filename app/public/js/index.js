04fa1373b294431c864ad8e482eacf3d07ff00df
adisahoo <adisahoo@iu.edu>
2021-10-29 10:10:06 -0400

    edit and delete changes

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -14,18 +14,10 @@ const Offer = {
     },
     
     methods: {
-      
-        fetchUserData(){
-            fetch('https://randomuser.me/api/')
-        .then(response => response.json())        
-        .then((parsedJson) => {            
-            console.log(parsedJson);
-            this.person = parsedJson.results[0]
-        })
-        .catch( err => {
-            console.error(err)
-        })
-        },
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },      
         fetchBookData() {
             fetch('/api/book/')
             .then( response => response.json() )
@@ -36,6 +28,36 @@ const Offer = {
             .catch( (err) => {
                console.error(err)
             })
+        },
+        postBook(evt) {
+            console.log ("Test:", this.selectedBook);
+          if (this.selectedBook) {
+              this.postEditBook(evt);
+          } else {
+              this.postNewBook(evt);
+          }
+        },
+        postEditBook(evt) {
+          this.bookForm.id = this.selectedBook.id;
+
+          console.log("Editing!", this.bookForm);
+  
+          fetch('api/book/update.php', {
+              method:'POST',
+              body: JSON.stringify(this.bookForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.books = json;
+              
+              // reset the form
+              this.handleResetEdit();
+            });
         },
         
         postNewBook(evt) {
@@ -56,16 +78,46 @@ const Offer = {
                 
                 // reset the form                
                 this.handleResetEdit();
+              })
+              .catch( err => {
+                alert("Something went horribly wrong.");              
               });
-           } ,
-           handleEditOffer(offer){
-            this.selectedOffer = offer;
-            this.offerForm = this.selectedOffer;
-       },
-       handleResetEdit() {
-           this.selectedOffer = null;
-           this.offerForm = {};
-       }
+        },
+
+           postDeleteBook(o) {  
+            if ( !confirm("Are you sure you want to delete " + o.title + " from the table?") ) {
+                return;
+            }  
+            
+            console.log("Delete!", o);
+    
+            fetch('api/book/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+          },
+
+          handleEditBook(book) {
+            this.selectedBook = book;
+            this.bookForm = this.selectedBook;
+          },
+          handleResetEdit() {
+               this.selectedBook = null;
+               this.bookForm = {};
+           }
+         
     },
     created(){          //event book funtion "created()" by vue.js allows you to attach to the event
         
